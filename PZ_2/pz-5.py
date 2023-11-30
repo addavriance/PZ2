18e2441e5a36bdaa9ef4d5f399cf76d45a7306e2
addavriance <61160742+addavriance@users.noreply.github.com>
2023-11-30 19:07:57 +0300

    .

---
+++ PZ_2/pz-5.py
@@ -0,0 +1,20 @@
+"""
+Вариант 19.
+
+Составить функцию решения задачи: из заданного числа вычли сумму его цифр.
+Из результата вновь вычли сумму его цифр и т. д. Через сколько таких действий получится нуль?
+"""
+
+
+def find_steps_to_zero(num):
+    steps = 0
+    while num > 0:
+        digit_sum = sum([int(digit) for digit in str(num)])
+        num -= digit_sum
+        steps += 1
+    return steps
+
+
+numb = int(input("Enter a number: "))
+
+print(find_steps_to_zero(numb))
