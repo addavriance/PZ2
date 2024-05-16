f894ce9ff814c5972a56edbd1475b370a7881521
addavriance <61160742+addavriance@users.noreply.github.com>
2024-05-16 22:42:12 +0300

    pz14

---
+++ PZ_2/pz-14.py
@@ -0,0 +1,13 @@
+"""
+Вариант 19
+Из исходного текстового файла (pazzl.html) выбрать все html-коды изображений.
+Посчитать их количество.
+"""
+
+import re
+
+with open("./assets/pazzl.html") as f:
+    text = f.read()
+    img_tags = re.findall(r"<img[^>]*>", text)
+    print(*img_tags)
+    print(f"Количество тегов 'img': {len(img_tags)}")
